4eb2fc41a3327c09597535b977c69bc0e80ad908
dimmoborgir <dimmoborgir@gmail.com>
2017-05-03 01:27:44 +0500

    no payload

--- checkers/doorlock/call-all-methods.sh
+++ checkers/doorlock/call-all-methods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-coap -T post -p 'x' 'coap://127.0.0.1/register_lock?model=MMOODDEELL&floor=6&room=616'
-coap -T post -p 'x' 'coap://127.0.0.1/add_card?lock=BBB&card=5432&tag=TTAAGG'
+echo -n | coap -T post 'coap://127.0.0.1/register_lock?model=MMOODDEELL&floor=6&room=616'
+echo -n | coap -T post 'coap://127.0.0.1/add_card?lock=BBB&card=5432&tag=TTAAGG'
 coap -T get 'coap://127.0.0.1/get_card?lock=AAA&card=1234'
