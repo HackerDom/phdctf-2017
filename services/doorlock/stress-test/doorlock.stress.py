093b67bd5cd885b85ae613ca100e2a6417366d45
dimmoborgir <dimmoborgir@gmail.com>
2017-05-06 01:19:14 +0500

    stress test: no 0 digits, count ok/err

--- services/doorlock/stress-test/doorlock.stress.py
+++ services/doorlock/stress-test/doorlock.stress.py
@@ -14,27 +14,29 @@ logging.basicConfig(
     filename='doorlock.stress.log'
 )
 
+COUNT_OK, COUNT_ERR = 0, 0
+
 def random_str(size, chars):
     return ''.join(random.choice(chars) for _ in range(size))
 
 async def make_task(host):
+    global COUNT_OK, COUNT_ERR
     client = DoorlockClient(host)
 
     model = random_str(4, string.ascii_uppercase)
-    floor = random_str(1, string.digits)
+    floor = random_str(1, "123456789")
     room = floor + random_str(3, string.digits)
-
     code, lock = await client.register_lock(model, floor, room)
 
-    await asyncio.sleep(1)
-
-    card = random_str(1, string.digits)
+    card = random_str(1, "123456789")
     tag  = random_str(32, string.ascii_uppercase)
     await client.add_card(lock, card, tag)
 
-    await asyncio.sleep(1)
     code, tag_check = await client.get_card(lock, card)
-    if tag != tag_check:
+    if tag == tag_check:
+        COUNT_OK += 1
+    else:
+        COUNT_ERR += 1
         logging.getLogger().error("Expected '%s', but got '%s'", tag, tag_check)
 
 if len(sys.argv) != 3:
@@ -54,5 +56,6 @@ req_per_count = 3
 sec = (datetime.datetime.now() - t_start).microseconds/1e6
 log_str = '%d RPS (%d req in %f sec)' % (req_per_count*count/sec, req_per_count*count, sec)
 logging.getLogger().info(log_str)
+logging.getLogger().info('OK: %d, ERR: %d', COUNT_OK, COUNT_ERR)
 
 loop.close()
