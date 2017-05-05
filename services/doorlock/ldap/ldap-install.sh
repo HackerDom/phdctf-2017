7f88e874690412026fb3bc1e4aff9a17c7df3798
dimmoborgir <dimmoborgir@gmail.com>
2017-05-06 01:18:42 +0500

    install fix file owner

--- services/doorlock/ldap/ldap-install.sh
+++ services/doorlock/ldap/ldap-install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -e
+
 service slapd stop
+
 cp -v doorlock.schema /etc/ldap/schema/doorlock.schema
-cp -v slapd.d/cn=config/cn=schema/cn={4}doorlock.ldif /etc/ldap/slapd.d/cn=config/cn=schema/cn={4}doorlock.ldif
+
+LDIF=slapd.d/cn=config/cn=schema/cn={4}doorlock.ldif
+cp -v $LDIF /etc/ldap/$LDIF
+chown openldap.openldap /etc/ldap/$LDIF
+
 service slapd start
+
 echo Success!
