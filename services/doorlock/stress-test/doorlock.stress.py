dc95fd8d330f7b58358dbf7a58839d5d2ffc5e19
dimmoborgir <dimmoborgir@gmail.com>
2017-05-06 01:08:05 +0500

    better stress test

---
+++ services/doorlock/stress-test/doorlock.stress.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import sys
+import logging
+import asyncio
+import string
+import random
+import datetime
+from doorlockclient import DoorlockClient
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)-15s|%(levelname)-5s|%(message)s',
+    filename='doorlock.stress.log'
+)
+
+def random_str(size, chars):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+async def make_task(host):
+    client = DoorlockClient(host)
+
+    model = random_str(4, string.ascii_uppercase)
+    floor = random_str(1, string.digits)
+    room = floor + random_str(3, string.digits)
+
+    code, lock = await client.register_lock(model, floor, room)
+
+    await asyncio.sleep(1)
+
+    card = random_str(1, string.digits)
+    tag  = random_str(32, string.ascii_uppercase)
+    await client.add_card(lock, card, tag)
+
+    await asyncio.sleep(1)
+    code, tag_check = await client.get_card(lock, card)
+    if tag != tag_check:
+        logging.getLogger().error("Expected '%s', but got '%s'", tag, tag_check)
+
+if len(sys.argv) != 3:
+    print('Usage: %s HOST COUNT' % sys.argv[0])
+    sys.exit(-1)
+
+host, count = sys.argv[1], int(sys.argv[2])
+
+loop = asyncio.get_event_loop()
+
+t_start = datetime.datetime.now()
+tasks = [make_task(host) for i in range(count)]
+
+loop.run_until_complete(asyncio.wait(tasks))
+
+req_per_count = 3
+sec = (datetime.datetime.now() - t_start).microseconds/1e6
+log_str = '%d RPS (%d req in %f sec)' % (req_per_count*count/sec, req_per_count*count, sec)
+logging.getLogger().info(log_str)
+
+loop.close()
---
+++ services/doorlock/stress-test/doorlockclient.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import logging
+import asyncio
+import datetime
+import aiocoap
+
+class DoorlockClient:
+    def __init__(self, host):
+        self._log = logging.getLogger()
+        self._base_url = 'coap://%s/' % host
+
+    def _build_uri(self, method, args):
+        uri = self._base_url + method
+        if args:
+            uri += '?'
+            separator = ''
+            for key, val in args.items():
+                uri += separator + key + '=' + str(val)
+                separator = '&'
+        return uri
+
+    async def _send(self, code, method, args):
+        protocol = await aiocoap.Context.create_client_context()
+        uri = self._build_uri(method, args)
+        request = aiocoap.Message(code=code, uri=uri)
+        self._log.info('--> %s', uri)
+        try:
+            t_start = datetime.datetime.now()
+            response = await protocol.request(request).response
+        except Exception as e:
+            t_diff = (datetime.datetime.now() - t_start).microseconds/1000
+            self._log.error('Exception: %s in %d ms', e, t_diff)
+            return -1, None
+        else:
+            t_diff = (datetime.datetime.now() - t_start).microseconds/1000
+            self._log.info('<-- [%s] %r in %d ms', response.code, response.payload, t_diff)
+            return response.code, response.payload.decode('ascii')
+
+    async def register_lock(self, model, floor, room):
+        return await self._send(aiocoap.POST, 'register_lock', {'model': model, 'floor': floor, 'room': room})
+
+    async def add_card(self, lock, card, tag):
+        return await self._send(aiocoap.POST, 'add_card', {'lock': lock, 'card': card, 'tag': tag})
+
+    async def get_card(self, lock, card):
+        return await self._send(aiocoap.GET, 'get_card', {'lock': lock, 'card': card})
