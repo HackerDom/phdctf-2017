15f1fe1f82805e64ba6f2abc0dc34881395cf334
ar7z1 <art@hackerdom.ru>
2017-05-06 14:35:41 +0300

    Auth topic subscription

--- services/thermometer/module/src/module.c
+++ services/thermometer/module/src/module.c
@@ -6,13 +6,13 @@
 #include <stdlib.h>
 #include <microhttpd.h>
 #include "MQTTClient.h"
-
 #include <sys/timeb.h>
+#include <signal.h>
 
-#define PORT        8888
-#define ADDRESS     "tcp://mqtt-broker:1883"
-#define CLIENTID    "MODULE1"
-#define QOS         1
+#define HTTP_SERVER_PORT            8888
+#define MQTT_BROKER_ADDRESS         "tcp://mqtt-broker:1883"
+#define MQTT_MODULE_CLIENT_ID       "temperature-module"
+#define QOS                         1
 #define AUTHORIZATION_REQUEST_TOPIC "house/authorization"
 
 int answer_to_connection (void *cls, struct MHD_Connection *connection,
@@ -63,28 +63,20 @@ int answer_to_connection (void *cls, struct MHD_Connection *connection,
     return ret;
 }
 
-void delivered(void *context, MQTTClient_deliveryToken dt)
+int process_mqtt_message(void *context, char *topic_name, int topic_len, MQTTClient_message *message)
 {
-    printf("Message with token value %d delivery confirmed\n", dt);
-}
-
-int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message)
-{
-    int i;
-    char* payloadptr;
-
-    printf("Message arrived\n");
-    printf("     topic: %s\n", topicName);
-    printf("   message: ");
-
-    payloadptr = message->payload;
-    for(i=0; i<message->payloadlen; i++)
+    if (strcmp(topic_name, AUTHORIZATION_REQUEST_TOPIC) == 0) 
     {
-        putchar(*payloadptr++);
+        printf("Authorization request for client_id '%s'.\n", message->payload);
     }
-    putchar('\n');
+    else
+    {
+        printf("Message from topic '%s' arrived.\n", topic_name);
+    }
+
     MQTTClient_freeMessage(&message);
-    MQTTClient_free(topicName);
+    MQTTClient_free(topic_name);
+
     return 1;
 }
 
@@ -94,73 +86,123 @@ void connlost(void *context, char *cause)
     printf("     cause: %s\n", cause);
 }
 
-int process_message(MQTTClient* client)
+void init_mqtt_client(MQTTClient* mqtt_client)
 {
-    char* topicName = NULL;
-    int topicLen;
-    MQTTClient_message* message = NULL;
     int rc;
 
-    rc = MQTTClient_receive(client, &topicName, &topicLen, &message, 1000);
-    
-    if (message)
+    if ((rc = MQTTClient_create(mqtt_client, MQTT_BROKER_ADDRESS, MQTT_MODULE_CLIENT_ID, MQTTCLIENT_PERSISTENCE_NONE, NULL)) != MQTTCLIENT_SUCCESS)
     {
-	printf("%s\t", topicName);
-	printf("%.*s", message->payloadlen, (char*)message->payload);
-	MQTTClient_freeMessage(&message);
-	MQTTClient_free(topicName);
+        printf("Failed to create MQTT client, return code %d.\n", rc);
+        exit(EXIT_FAILURE);
     }
-
-    return rc;
 }
 
-int main ()
+void set_mqtt_callbacks(MQTTClient* mqtt_client)
 {
-    MQTTClient client;
-    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-    //MQTTClient_deliveryToken deliveredtoken;
     int rc;
-    int ch;
 
-    MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    if ((rc = MQTTClient_setCallbacks(*mqtt_client, NULL, NULL, process_mqtt_message, NULL)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to set callbacks for MQTT client, return code %d.\n", rc);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void connect_to_mqtt_broker(MQTTClient* mqtt_client)
+{
+    int rc;
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
     conn_opts.keepAliveInterval = 20;
     conn_opts.cleansession = 0;
+    conn_opts.connectTimeout = 10;
 
-    MQTTClient_setCallbacks(client, NULL, NULL, msgarrvd, NULL);
-
-    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
+    if ((rc = MQTTClient_connect(*mqtt_client, &conn_opts)) != MQTTCLIENT_SUCCESS)
     {
-        printf("Failed to connect, return code %d\n", rc);
+        printf("Failed to connect to MQTT broker '%s', return code %d.\n", MQTT_BROKER_ADDRESS, rc);
         exit(EXIT_FAILURE);
     }
 
-    rc = MQTTClient_subscribe(client, "#", 0);
+    printf("Connected to '%s'\n", MQTT_BROKER_ADDRESS);
+}
 
-    sleep(20000);
+void disconnect_from_mqtt_broker(MQTTClient* mqtt_client)
+{
+    int rc;
 
-    rc = MQTTClient_unsubscribe(client, "#");
+    if ((rc = MQTTClient_disconnect(*mqtt_client, 10000)) != MQTTCLIENT_SUCCESS)
+        printf("Failed to disconnect MQTT client, return code %d.\n", rc);
+    else
+        printf("Disconnected from '%s'.\n", MQTT_BROKER_ADDRESS);
+}
 
-    //do
-    //{
-        //rc = process_message(&client);
-        //printf("Processing result: %d\n", rc);
+void subscribe_to_topic(MQTTClient* mqtt_client, char *topic)
+{
+    int rc;
 
-        //if (rc == -1)
-        //{
-        //    MQTTClient_connect(client, &conn_opts);
-        //}
-    //}
-    //while(1);
+    if ((rc = MQTTClient_subscribe(*mqtt_client, AUTHORIZATION_REQUEST_TOPIC, 0)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to subscribe to topic '%s', return code %d.\n", AUTHORIZATION_REQUEST_TOPIC, rc);
+        exit(EXIT_FAILURE);
+    }
 
-    //struct MHD_Daemon *daemon;
+    printf("Subscribed to topic '%s'\n", AUTHORIZATION_REQUEST_TOPIC);
+}
 
-    //daemon = MHD_start_daemon (MHD_USE_SELECT_INTERNALLY, PORT, NULL, NULL, &answer_to_connection, NULL, MHD_OPTION_END);
-    //if (NULL == daemon) return 1;
+void unsubscribe_from_topic(MQTTClient* mqtt_client, char *topic)
+{
+    int rc;
 
-    //MHD_stop_daemon (daemon);
+    if ((rc = MQTTClient_unsubscribe(*mqtt_client, topic)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to unsubscribe from topic '%s', return code %d.\n", topic, rc);
+        exit(EXIT_FAILURE);
+    }
 
-    MQTTClient_disconnect(client, 10000);
-    MQTTClient_destroy(&client);
+    printf("Unsubscribed from topic '%s'.\n", topic);
+}
+
+volatile sig_atomic_t stop = 0;
+
+void term(int signum)
+{
+    stop = 1;
+}
+
+void subscribe_to_signals()
+{
+    struct sigaction action;
+    memset(&action, 0, sizeof(struct sigaction));
+    action.sa_handler = term;
+    sigaction(SIGTERM, &action, NULL);
+    sigaction(SIGINT, &action, NULL);
+}
+
+int main()
+{
+    subscribe_to_signals();
+
+    MQTTClient mqtt_client;
+    init_mqtt_client(&mqtt_client);
+    set_mqtt_callbacks(&mqtt_client);
+    connect_to_mqtt_broker(&mqtt_client);
+    subscribe_to_topic(&mqtt_client, AUTHORIZATION_REQUEST_TOPIC);
+
+    struct MHD_Daemon *daemon;
+    daemon = MHD_start_daemon (MHD_USE_SELECT_INTERNALLY, HTTP_SERVER_PORT, NULL, NULL, &answer_to_connection, NULL, MHD_OPTION_END);
+    if (NULL == daemon)
+        goto stop;
+
+    while (!stop)
+    {
+        sleep(10000);
+    }
+
+    MHD_stop_daemon (daemon);
+
+stop:
+    unsubscribe_from_topic(&mqtt_client, AUTHORIZATION_REQUEST_TOPIC);
+    disconnect_from_mqtt_broker(&mqtt_client);
+    MQTTClient_destroy(&mqtt_client);
 
     return 0;
 }
