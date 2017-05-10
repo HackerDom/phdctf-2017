3b6b240dd8dc7deeb24cd6e5ff7c2901d014607b
dimmoborgir <dimmoborgir@gmail.com>
2017-05-11 01:25:19 +0500

    [Doorlock] Run ldap-dpkg-reconfigure.sh once to populate 'ldap' volume

--- services/doorlock/docker-wrapper.sh
+++ services/doorlock/docker-wrapper.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+[ -f /var/lib/ldap/DB_CONFIG ] || /app/ldap-dpkg-reconfigure.sh  # to populate 'ldap' volume
+
 /usr/sbin/slapd -h ldap://127.0.0.1:389/ -g openldap -u openldap -F /etc/ldap/slapd.d -d Config &
 status=$?
 if [ $status -ne 0 ]; then
