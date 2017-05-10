68530ae7996aa3c8ae9eb1dca128d2245c3d01a8
dimmoborgir <dimmoborgir@gmail.com>
2017-05-11 01:59:03 +0500

    [Doorlock] docker-wrapper.sh cleanup + better waiting for slapd start

--- services/doorlock/docker-wrapper.sh
+++ services/doorlock/docker-wrapper.sh
@@ -3,35 +3,14 @@
 [ -f /var/lib/ldap/DB_CONFIG ] || /app/ldap-dpkg-reconfigure.sh  # to populate 'ldap' volume
 
 /usr/sbin/slapd -h ldap://127.0.0.1:389/ -g openldap -u openldap -F /etc/ldap/slapd.d -d Config &
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start slapd: $status"
-  exit $status
-fi
-echo Started slapd
 
-sleep 5
+while ! nc -z 127.0.0.1 389
+do
+    sleep 1
+    echo "Waiting for slapd ... "
+done
 
 /app/ldap-init.sh
 
 /app/doorlock-server
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start doorlock-server: $status"
-  exit $status
-fi
-echo Started doorlock-server
 
-# Naive check runs checks once a minute to see if either of the processes exited.
-# This illustrates part of the heavy lifting you need to do if you want to run
-# more than one service in a container. The container will exit with an error
-# if it detects that either of the processes has exited.
-while /bin/true; do
-  PROCESS_1_STATUS=$(ps aux |grep -q slapd)
-  PROCESS_2_STATUS=$(ps aux |grep -q doorlock-server)
-  if [ $PROCESS_1_STATUS || $PROCESS_2_STATUS ]; then
-    echo "One of the processes has already exited."
-    exit -1
-  fi
-  sleep 60
-done
